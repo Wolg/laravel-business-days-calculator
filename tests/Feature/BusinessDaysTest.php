80124482d929b5571c762105bcb1506446140b68
Stanislav Chistenko <skvest1@gmail.com>
2019-06-03 14:07:17 +0600

    Added smoke test

---
+++ tests/Feature/BusinessDaysTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BusinessDaysTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function smoke_test()
+    {
+        $response = $this->get('api/businessDates/getBusinessDateWithDelay?initialDate=2018-12-12T10:10:10Z&delay=3');
+        $response->assertStatus(200);
+
+        $response = $this->post('api/businessDates/getBusinessDateWithDelay', ['initialDate' => '2018-12-12T10:10:10Z', 'delay' => 3], []);
+        $response->assertStatus(200);
+    }
+}
