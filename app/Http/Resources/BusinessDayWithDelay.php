41cc5d6f707cf1e15ef44a7ced1b11c608892fa4
Stanislav Chistenko <skvest1@gmail.com>
2019-06-03 15:28:07 +0600

    Added draft for BusinessDayWithDelay resource

---
+++ app/Http/Resources/BusinessDayWithDelay.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BusinessDayWithDelay extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'ok' => true,
+            'initialQuery' => $request->all(),
+            'results' => [
+
+            ]
+        ];
+    }
+}
